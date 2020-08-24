2cc6ff78a857b4db83ba8d469e763fa547f4b73f
EPaetow <elipaetow@yahoo.com>
2020-08-24 10:41:14 -0500

    update

--- eli_paetow_code/project0/src/main/java/com/revature/repos/UserRepository.java
+++ eli_paetow_code/project0/src/main/java/com/revature/repos/UserRepository.java
@@ -1,8 +1,11 @@
 package com.revature.repos;
 
 import com.revature.models.AppUser;
+import com.revature.util.ConnectionFactory;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
@@ -21,11 +24,25 @@ public class UserRepository {
      * talk to the DB and run SQL querys
      * */
 
-    public Optional<AppUser> _user = Optional.empty();
+    public Optional<AppUser> findUserByCredentials(String username, String password) {
 
-    try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+        Optional<AppUser> _user = Optional.empty();
 
-    }catch(SQLException sqle){
-        System.out.println("there was a SQL exception");
-    }return _user;
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            String sql = "SELECT * FROM project0.app_users au " +
+                                "JOIN project0.user_roles ur " +
+                                "ON au.role_id = ur.id ";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+
+            ResultSet rs = pstmt.executeQuery();
+            _user = mapResultSet(rs).stream().findFirst();
+
+        } catch (SQLException sqle) {
+            System.out.println("there was a SQL exception");
+        }
+        return _user;
+    }
 }
--- eli_paetow_code/project0/src/main/java/com/revature/util/ConnectionFactory.java
+++ eli_paetow_code/project0/src/main/java/com/revature/util/ConnectionFactory.java
@@ -1,4 +1,68 @@
 package com.revature.util;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class ConnectionFactory {
+
+    /*
+     * create a singleton
+     * */
+
+    private static ConnectionFactory connFactory = new ConnectionFactory();
+
+
+    private Properties props = new Properties();
+
+    private ConnectionFactory() {
+        try {
+            props.load(new FileReader("./src/main/resources/application.properties"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /*
+     * set up getters tp work with connection factory since it is private
+     * */
+
+    public static ConnectionFactory getInstance(){
+        return connFactory;
+    }
+
+    public Connection getConnection(){
+
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            //create a connection here to dbeaver postgres
+            conn = DriverManager.getConnection(
+                    props.getProperty("url") ,
+                    props.getProperty("username") ,
+                    props.getProperty("password"));
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        if (conn == null) {
+            throw new RuntimeException("Failed to establish connection.");
+        }
+
+        return conn;
+
+    }
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+
 }
+
