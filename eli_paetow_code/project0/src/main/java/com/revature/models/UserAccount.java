1f0b86198ebb05b94c83b066b6c7770c8bfbf3f8
EPaetow <elipaetow@yahoo.com>
2020-08-24 19:11:44 -0500

    account repo update

---
+++ eli_paetow_code/project0/src/main/java/com/revature/models/UserAccount.java
@@ -0,0 +1,85 @@
+package com.revature.models;
+
+import java.util.Objects;
+
+public class UserAccount {
+    /*
+     * POJO for account
+     * */
+    private Integer id;
+    private Double balance;
+    private Integer user_id;
+
+
+    /*
+     * constructor for all fields
+     * */
+    public UserAccount(Integer id, Double balance, Integer user_id) {
+        this.id = id;
+        this.balance = balance;
+        this.user_id = user_id;
+    }
+
+    /*
+     * copy constructor to create a new instance with values
+     * */
+    public UserAccount(UserAccount copy){
+        this(copy.id, copy.balance, copy.user_id);
+    }
+
+    /*
+     * getters and setters
+     * */
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    /*
+     * Overrides
+     * */
+
+    @Override
+    public String toString() {
+        return "UserAccount{" +
+                "id=" + id +
+                ", balance=" + balance +
+                ", user_id=" + user_id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserAccount that = (UserAccount) o;
+        return Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getBalance(), that.getBalance()) &&
+                Objects.equals(getUser_id(), that.getUser_id());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getBalance(), getUser_id());
+    }
+}
---
+++ eli_paetow_code/project0/src/main/java/com/revature/repos/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.revature.repos;
+
+public class AccountRepository {
+
+    public AccountRepository(){
+        System.out.println("Account repo is running");
+    }
+
+
+}
